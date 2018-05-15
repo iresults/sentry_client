05b76bcc7b19190e9e8033818d1c04affdef9921
Thomas Greber <greber@comsolit.com>
2018-05-15 09:29:42 +0200

    [STGAG-7474] add type hint for exception parameter

--- Classes/DebugExceptionHandler.php
+++ Classes/DebugExceptionHandler.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cod
- * Date: 16.12.16
- * Time: 14:15
- */
 
 namespace SentryClient;
-
 
 class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler
 {
@@ -15,11 +8,10 @@ class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler
      * Displays the given exception
      *
      * @param \Exception|\Throwable $exception The exception(PHP 5.x) or throwable(PHP >= 7.0) object.
-     * @TODO #72293 This will change to \Throwable only if we are >= PHP7.0 only
      *
      * @throws \Exception
      */
-    public function handleException($exception)
+    public function handleException(\Throwable $exception)
     {
         ClientProvider::captureException($exception);
 
--- Classes/ProductionExceptionHandler.php
+++ Classes/ProductionExceptionHandler.php
@@ -1,25 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cod
- * Date: 16.12.16
- * Time: 14:15
- */
 
 namespace SentryClient;
-
 
 class ProductionExceptionHandler extends \TYPO3\CMS\Core\Error\ProductionExceptionHandler
 {
     /**
-     * Displays the given exception
-     *
-     * @param \Exception|\Throwable $exception The exception(PHP 5.x) or throwable(PHP >= 7.0) object.
-     * @TODO #72293 This will change to \Throwable only if we are >= PHP7.0 only
-     *
-     * @throws \Exception
+     * @param \Exception|\Throwable $exception
      */
-    public function handleException($exception)
+    public function handleException(\Throwable $exception)
     {
         ClientProvider::captureException($exception);
 
